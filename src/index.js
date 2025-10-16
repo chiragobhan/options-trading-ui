907997d432c6dadba6db8095b5da7afadbed77ba
Chirag Obhan <32812640+chiragobhan@users.noreply.github.com>
2025-10-15 21:27:48 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppLayout from './components/Layout/AppLayout';
-
-ReactDOM.render(
-  <AppLayout />,
-  document.getElementById('betmount')
-);
